0fe2b7c842ceb43a4739ea72cdd95e878564a285
gander393 <85196167+gander393@users.noreply.github.com>
2021-06-01 16:28:30 -0400

    Create Trumpet.java

---
+++ Trumpet.java
@@ -0,0 +1,13 @@
+public class Trumpet extends Instrument{
+
+	@Override
+	public void makeSound() {
+		System.out.println("brumm brumm");
+		
+	}
+
+	public static void main(String[] args) {
+		Trumpet trumpet = new Trumpet();
+		trumpet.makeSound();
+	}
+}
