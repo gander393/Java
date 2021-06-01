099061bb0ecd888b3c4ab31e57928c26ed4dd54b
gander393 <85196167+gander393@users.noreply.github.com>
2021-06-01 16:28:08 -0400

    Create Instrument.java

---
+++ Instrument.java
@@ -0,0 +1,14 @@
+public abstract class Instrument {
+
+	private String brand;
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	
+	public abstract void makeSound();
+}
