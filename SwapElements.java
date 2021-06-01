826ef4a1877ea6c2e671c879c94124da4ad80ec9
gander393 <85196167+gander393@users.noreply.github.com>
2021-06-01 16:36:57 -0400

    Create SwapElements.java

---
+++ SwapElements.java
@@ -0,0 +1,10 @@
+class SwapElements {
+
+	public static void main(String[] args) {
+		int[] elements = {1, 2, 3, 4, 5};
+		elements[0] = 5;
+		elements[4] = 1;
+		System.out.println(elements[0]);
+		System.out.println(elements[4]);
+	}
+}
