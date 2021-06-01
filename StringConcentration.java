fdb951f2339a8cadda57be062f5b7195e275ca8c
gander393 <85196167+gander393@users.noreply.github.com>
2021-06-01 17:18:26 -0400

    Create StringConcentration.java

---
+++ StringConcentration.java
@@ -0,0 +1,23 @@
+public class StringConcatenation {
+
+	public static void main(String[] args) {
+		String beginning = "Hello";
+		String end = " World!";
+		String combo = beginning + end;
+		
+		System.out.println(combo);
+		
+		long l = 203L;
+		
+		System.out.println("the Value of l: " + l);
+		
+		boolean b = true;
+		
+		System.out.println(b + " is true");
+		
+		int x = 2;
+		int y = 3;
+		
+		System.out.println("the sum of x and y is " + (x + y));
+	}
+}
