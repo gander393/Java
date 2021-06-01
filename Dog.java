7817d5aeddec5da1c87bdb9fd49cfcd1659caa32
gander393 <85196167+gander393@users.noreply.github.com>
2021-06-01 16:21:44 -0400

    Create Dog.java

---
+++ Dog.java
@@ -0,0 +1,18 @@
+public class Dog {
+	String name;
+
+	//getter
+	public String getName() {
+		return name;
+	}
+
+	//setter
+	public void setName(String name) {
+		this.name = name;
+	}	
+	
+	public void talk() {
+		System.out.println("woof");
+	}
+	
+}
