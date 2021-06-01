e73a198315f56b85557fdfcea8457e1448a1fa80
gander393 <85196167+gander393@users.noreply.github.com>
2021-06-01 16:30:33 -0400

    Create ExampleOne7.java

---
+++ ExampleOne7.java
@@ -0,0 +1,11 @@
+public class ExampleOne {
+
+	public static void main(String[] args) {
+		String name = "Jessica";
+		printName(name);
+	}
+	
+	public static void printName(String name) {
+		System.out.println("Hello " + name);
+	}
+}
