e3b86a68e9e256000a030edb06f347afdb3bb40c
gander393 <85196167+gander393@users.noreply.github.com>
2021-06-01 16:27:38 -0400

    Create MyFirstProgram.java

---
+++ MyFirstProgram.java
@@ -0,0 +1,14 @@
+package examples;
+
+public class MyFirstProgram {
+	
+	public static void message(String myString, int myInt) {
+		System.out.println("This is my string: " + myString);
+		System.out.println("and this is my int: " + myInt);
+	}
+
+	public static void main(String[] args) {
+		message("Hello world!", 4);
+	}
+
+}
